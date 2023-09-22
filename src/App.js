c40ed4171de6af2fd4efedb8810983b97870b75f
Nora Sadder <norasadder12@gmail.com>
2023-09-22 15:13:43 +0300

    add note section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 function Header() {
@@ -7,12 +8,54 @@ function Header() {
 
       <div className="search-bar">
         <img src={require("./images/search.png")}></img>
-        <input className="search-input" placeholder="Search"></input>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+        ></input>
       </div>
     </div>
   );
 }
 
+function AddNote() {
+  const [isVisisble, setIsVisisble] = useState(false);
+  function handleOnClick() {
+    setIsVisisble(!isVisisble);
+  }
+  return (
+    <div>
+      {!isVisisble && (
+        <div className="add-note-container">
+          <button onClick={handleOnClick}>Take a note..</button>
+        </div>
+      )}
+      {isVisisble && (
+        <form className="add-note-form">
+          <input
+            type="text"
+            className="add-note-title"
+            placeholder="Title"
+          ></input>
+          <input
+            type="text"
+            className="add-note-content"
+            placeholder="Take a note..."
+          ></input>
+          <div className="add-note-close">
+            <input type="submit" value="close" onClick={handleOnClick}></input>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+}
+
 export default function App() {
-  return <Header />;
+  return (
+    <div>
+      <Header /> ;
+      <AddNote />;
+    </div>
+  );
 }
